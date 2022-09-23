0dcd97a0aa85d5d45c98e816c7078236e4c60b88
michalbendavid <michalbendavid770@gmail.com>
2022-09-23 13:15:06 +0300

    fix  ex5!! :)

--- 5-ex5/ex5.js
+++ 5-ex5/ex5.js
@@ -1,32 +1,25 @@
 const characters = require("../data/harry_potter/characters.json");
 const solution = require("../solutions/solution5");
-
 // filter all characters that are
 // 1. male
 // 2. human
 // 3. belong to one of the house (the house should not be an empty field)
-
 // and present an object only with the name and house
-/*
-    {
+/*   {
         name,
         house,
-    }
-*/
-
+    }*/
 // HINT: there should only be 83 of them
+let formattedCharacters = characters.filter((character) => 
+    (character.gender === "male" && character.species === "human" &&
+    character.house)
+     );
+formattedCharacters = formattedCharacters.map((character) => {
+  return { name : character.name, house: character.house};
+  });
+  
+console.log(formattedCharacters);
 
-const formattedCharacters = characters.map((character) => {
-  if (
-    (character.gender == "male" && character.species == "human") ||
-    character.house !== null
-  ) {
-    return {
-      name: character.name,
-      house: character.house,
-    };
-  }
-});
 
 console.log(
   "Exercie 5 passed: ",
