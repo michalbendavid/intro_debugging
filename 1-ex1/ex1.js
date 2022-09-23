9d297a2efafa6027a06185611b18e90098fa8d5f
michalbendavid <michalbendavid770@gmail.com>
2022-09-23 10:10:18 +0300

    fix ex1 and ex2

--- 1-ex1/ex1.js
+++ 1-ex1/ex1.js
@@ -2,8 +2,8 @@ const characters = require("../data/harry_potter/characters.json");
 const solution = require("../solutions/solution1");
 
 // present all character from the house Gryffindor
-const gryffindors = characters.filter((x) => x.house === "gryffindor");
-
+const gryffindors = characters.filter((x) => x.house === "Gryffindor");
+console.log(gryffindors);
 console.log(
   "Exercie 1 passed: ",
   JSON.stringify(gryffindors) === JSON.stringify(solution)
--- 2-ex2/ex2.js
+++ 2-ex2/ex2.js
@@ -2,10 +2,9 @@ const characters = require("../data/harry_potter/characters.json");
 const solution = require("../solutions/solution2");
 // present all character from the house Gryffindor
 
-const gryffindors = characters.filter((x) => {
-  x.house === "Gryffindor";
-});
-
+const gryffindors = characters.filter((x) => 
+  x.house === "Gryffindor");
+console.log(gryffindors.house);
 console.log(
   "Exercie 2 passed: ",
   JSON.stringify(gryffindors) === JSON.stringify(solution)
